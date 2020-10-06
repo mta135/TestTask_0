34f651454d0e01a70ac7ee86d1c634e180695f6b
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-06 17:58:51 +0300

    someComit_1

---
+++ TestTask_1/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask_1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [NotMapped]
+        public Review Review { get; set; }
+    }
+}
