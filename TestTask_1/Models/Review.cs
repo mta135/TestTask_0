9b8b82f4882146a25a039d20cda0effff45ef522
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-09 15:31:22 +0300

    Add Required Properties

--- TestTask_1/Models/Review.cs
+++ TestTask_1/Models/Review.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,9 +14,13 @@ namespace TestTask_1.Models
         public int Id { get; set; }
 
         [DisplayName("Name of Review")]
+        [Required(ErrorMessage = "Mandatory Field. Input length must be not longer than 100 characters.")]
+        [StringLength(100)]
         public string Name { get; set; }
 
         [DisplayName("Descprition of Review")]
+        [Required(ErrorMessage = "Mandatory Field. Input length must be not longer than 100 characters. ")]
+        [StringLength(100)]
         public string Description { get; set; }
         public int CategoryId { get; set; }
 
