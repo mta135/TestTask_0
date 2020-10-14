c0429b149bb0845d795c45f2ce37ce3a31c0c934
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-14 20:48:47 +0300

    Add ICityDetails interface

---
+++ TestTask_2/Infrastructure/ICityDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask_2.Models;
+using TestTask_2.ViewModels;
+
+namespace TestTask_2.Infrastructure
+{
+    public interface ICityDetails
+    {
+        void SaveCityDetails(CityDetailsViewModel cityDetailsViewModel);
+        IEnumerable<City> Cities { get; }
+        IEnumerable<Street> Streets { get; }
+        IEnumerable<CityDetail> CityDetails { get; }
+        IEnumerable<StreetCityDetail> StreetCityDetails { get; }
+    }
+}
