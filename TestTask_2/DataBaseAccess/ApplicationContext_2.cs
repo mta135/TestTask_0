6f95d26b275e6b8cb5928cfa01bfb5091c27dde2
Tamazlicaru, Mihai <Mihai.Tamazlicaru@dsi.md>
2020-10-14 20:43:08 +0300

    add Streets, CityDetails, StreetCityDetails clases

--- TestTask_2/DataBaseAccess/ApplicationContext_2.cs
+++ TestTask_2/DataBaseAccess/ApplicationContext_2.cs
@@ -9,10 +9,11 @@ namespace TestTask_2.DataBaseAccess
 {
     public class ApplicationContext_2 : DbContext
     {
- 
         public DbSet<User> Users { get; set; }
         public DbSet<City> Cities { get; set; }
-
+        public DbSet<Street> Streets { get; set; }
+        public DbSet<CityDetail> CityDetails { get; set; }
+        public DbSet<StreetCityDetail> StreetCityDetails { get; set; }
 
         public ApplicationContext_2(DbContextOptions<ApplicationContext_2> options) : base(options) { }
     }
